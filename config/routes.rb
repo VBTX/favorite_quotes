bfe2891ec108e60a7675d4aa9263270f1acfc48f
VBTX <jv.svedaite@gmail.com>
2019-07-24 17:51:54 -0500

    move routes under api v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :quotes
+
 namespace :api do
  namespace :v1 do	
   resources :users
+  resources :notes
+  resources :quotes
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
