d0b6b85c80c8a45df2e57520807d30d9e2196c57
VBTX <jv.svedaite@gmail.com>
2019-07-25 09:58:14 -0500

    set to find user by email for session creation

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
 	def create
-		@user = User.find_by(username: params[:session][:username])
+		@user = User.find_by(email: params[:session][:email])
 
 		if @user && @user.authenticate(params[:session][:password])
 			session[:user_id] = @user.id
