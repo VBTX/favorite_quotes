51f0280fb72baf80075507a68554595a74e04fcc
VBTX <jv.svedaite@gmail.com>
2019-07-25 06:42:32 -0500

    add create action to sessions controller and also put it under namespace

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,15 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
+	def create
+		@user = User.find_by(username: params[:session][:username])
+
+		if @user && @user.authenticate(params[:session][:password])
+			session[:user_id] = @user.id
+			# setting a user id for the session which is grabbed by app controller
+			render json: @user, status: 200
+		else
+			render json: {
+				error: "Invalid Credentials"
+			}
+	end
+end
 end
