6c770f08d3477682151190ad346845f256a2115c
VBTX <jv.svedaite@gmail.com>
2019-11-05 23:03:41 -0600

    change cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://goofy-villani-320adb.netlify.com'
+    origins 'https://favorite-quotes.netlify.com'
 
     resource '*',
       headers: :any,
