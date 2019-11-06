9d773f3e9f1376b68854d9ca2416a1aa4cbc8ee3
VBTX <jv.svedaite@gmail.com>
2019-11-05 22:19:28 -0600

    add diff cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://favorite-quotes-app.herokuapp.com'
+    origins 'https://goofy-villani-320adb.netlify.com'
 
     resource '*',
       headers: :any,
